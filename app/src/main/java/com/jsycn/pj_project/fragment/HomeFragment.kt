858448f30d958f740171233038246341fa2cef32
jiang.peng <jiang.peng@yintech.cn>
2021-01-14 15:04:50 +0800

    其他测试

--- app/src/main/java/com/jsycn/pj_project/fragment/HomeFragment.kt
+++ app/src/main/java/com/jsycn/pj_project/fragment/HomeFragment.kt
@@ -1,11 +1,14 @@
 package com.jsycn.pj_project.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.jsycn.pj_project.MainActivity
 import com.jsycn.pj_project.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 /**
  *@Description:
@@ -18,13 +21,11 @@ class HomeFragment: LazyFragmentNew() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-
-    override fun onHiddenChanged(hidden: Boolean) {
-        super.onHiddenChanged(hidden)
-    }
-
-    override fun setUserVisibleHint(isVisibleToUser: Boolean) {
-        super.setUserVisibleHint(isVisibleToUser)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bt_old_main.setOnClickListener {
+            startActivity(Intent(activity,MainActivity::class.java))
+        }
     }
 
 }

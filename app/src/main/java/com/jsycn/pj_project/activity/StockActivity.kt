78b4861050dcdda96a71819745d3290d779e104b
jiang.peng <jiang.peng@yintech.cn>
2021-02-09 16:40:42 +0800

    其他测试

---
+++ app/src/main/java/com/jsycn/pj_project/activity/StockActivity.kt
@@ -0,0 +1,17 @@
+package com.jsycn.pj_project.activity
+
+/**
+ * Author: jsync
+ * Date: 2021/2/9 15:25
+ * Description: 股票相关
+ * 长期：
+ * 用于股票：买入点  卖出点 对应  ，可以有多只股票
+ * 计算买入点和卖出点的差价，计算盈亏，底仓不动，底仓一只保持2w，一只股票的底仓价干到0后换下一只，预期值。
+ *
+ * 短期：
+ * 严格规定涨跌幅的买入卖出，到规定点必须抛售
+ *
+ * History:
+ */
+class StockActivity {
+}
